826cc10984b485f5a027b705ef6c0e41b7e11c25
chitrungtran <97813268+chitrungtran@users.noreply.github.com>
2025-11-24 15:58:37 +0700

    Update main.jsx

--- scr/main.jsx
+++ scr/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Tao đã bỏ cái <React.StrictMode> đi rồi, giờ nó chạy 1 lần thôi cho mượt
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
